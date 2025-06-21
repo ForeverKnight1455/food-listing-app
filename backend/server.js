7aa9eec20dc37df4c6bb63922308522076eb5049
Sreyas P Pradeep <sreyasinemily@gmail.com>
2025-06-21 13:43:36 +0530

    cleaned up code base

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 import express from 'express';  
 import { dbconnect } from './config/db.js';
 import productrouter from './route/product.route.js';  
-
+import path from "path"
 const app = express();
 app.use(express.json());
+const __dirname = path.resolve()
 app.use('/api/products', productrouter);
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 app.get('/', (req, res) => {
   res.send('Welcome to the Product API');
 });
