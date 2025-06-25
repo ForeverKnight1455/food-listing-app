6509c2ab348f750d059a8a95672b9e8f85c295de
Sreyas P Pradeep <sreyasinemily@gmail.com>
2025-06-25 22:23:55 +0530

    made changes in the routes

--- backend/route/product.route.js
+++ backend/route/product.route.js
@@ -4,8 +4,8 @@ import { createProduct,updateProduct,deleteProduct,getProducts } from '../contro
 const productrouter = express.Router();
 
 productrouter.get('/', getProducts);
-productrouter.delete('/:id', deleteProduct);
 productrouter.post('/', createProduct);
 productrouter.patch('/:id', updateProduct);
+productrouter.delete('/:id', deleteProduct);
 
 export default productrouter;
